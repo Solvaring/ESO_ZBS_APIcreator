cb385735d6b6c6cd233c910a4751b606a606b3fe
Solvaring <brandon.wall0711@gmail.com>
2017-07-08 11:12:35 -0700

    Removed esoxml.lua and renamed to esoxmlkeywordlist, updated xmlapicreator

--- xmlapicreator2.lua
+++ xmlapicreator2.lua
@@ -1,13 +1,26 @@
 local esouixml = io.open('esouixml.txt', 'r')
 local xmlstring = esouixml:read("*a")
-local working2 = io.open("ESOXML.lua", "w+")
+local working2 = io.open("ESOXMLKeywordList.lua", "w+")
 
 esouixml:seek('set')
 
 local keywordstring =""
 
-for keyword in xmlstring:gmatch("%b%*%*") do
-    keywordstring = keywordstring .. keyword
+for line in esouixml:lines() do
+    if line:find("h5. AlphaAnimation",1,true) then
+        break
+    end
+    local keyword = line:match("*%s([%a]+)%s*")
+    if keyword then
+        keywordstring = keywordstring .. " " .. keyword
+    end
+end
+
+for line in esouixml:lines() do
+    local keyword2 = line:match("h5.%s([%a%d]+)")
+    if keyword2 then
+        keywordstring = keywordstring .. " " .. keyword2
+    end
 end
 
 working2:write(keywordstring)
